0cdedf0e104d3d199ab5f3597131fbf3057a75df
Roberto Carbajales <robertocd98@gmail.com>
2020-05-06 10:26:48 -0400

    add var ok

--- routes/hospital.js
+++ routes/hospital.js
@@ -31,6 +31,7 @@ app.get('/', (req, res, next) => {
                     });
                 }
                 if (null) {
+                    let ok = 'chorizard';
                     console.log('aja');
                 }
 
